3ed988d23585089451a754a42fd1a0d6d951f47e
ykcai <lordsunday11@gmail.com>
2019-03-11 18:22:26 -0400

    wip login integration

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -9,16 +9,43 @@ export default Controller.extend({
     toggleModal() {
       this.toggleProperty("isActive");
     },
-    login() {
+    githubLogin() {
       if (!this.get("session").isAuthenticated)
         this.get("session").authenticate("authenticator:torii", "github");
+      if (this.get("session").isAuthenticated) this.transitionToRoute("home");
+    },
+    emailLogin() {
+      if (!this.get("session").isAuthenticated)
+        this.get("session").authenticate("authenticator:email", "email");
       if (this.get("session").isAuthenticated) this.transitionToRoute("home");
     },
     toggleSignUp() {
       this.toggleProperty("showSignUp");
     },
     createUser() {
-      //TODO
+      // POST users
+      Ember.$.ajax({
+        url: "api.tstfy.co/users",
+        type: "POST",
+        data: JSON.stringify({
+          username: "",
+          email: "",
+          password: "",
+          f_name: "",
+          l_name: "",
+          role: 0
+        })
+      })
+        .then(function(resp) {
+          // handle your server response here
+          console.log(resp);
+        })
+        .then(this.actions.emailLogin)
+        .catch(function(error) {
+          // handle errors here
+          console.log(error);
+        });
+      // then emailLogin()
     }
   }
 });
