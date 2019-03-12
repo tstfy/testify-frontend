d0373f17cf979f8cca478ba1055ee1dc649edffd
Michael Cai <ykcai@uwaterloo.ca>
2019-03-12 17:12:27 -0400

    added copy git repo with tooptip

--- app/components/candidate-list.js
+++ app/components/candidate-list.js
@@ -1,13 +1,3 @@
 import Component from "@ember/component";
 
-export default Component.extend({
-  status: "N/A",
-  actions: {
-    onSuccess() {
-      this.set("status", "Success");
-    },
-    onError() {
-      this.set("status", "Error");
-    }
-  }
-});
+export default Component.extend({});
