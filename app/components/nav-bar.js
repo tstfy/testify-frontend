3f7daabc1f0c8ede62894cfa15ac7e65e85f77ba
Michael Cai <ykcai@uwaterloo.ca>
2019-03-13 12:11:25 -0400

    added challenges hook. removed dates from challenges summary on home page

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,6 +2,7 @@ import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 
 export default Component.extend({
+  isLoading: false,
   showModal: false,
   showMenu: false,
   showNewChallenge: false,
@@ -17,8 +18,55 @@ export default Component.extend({
       this.toggleProperty("showModal");
       this.toggleProperty("showNewChallenge");
     },
-    saveChallenge() {
+    setSelection(value) {
+      this.set("category", value);
+    },
+    clearFields() {
+      this.set("category", "");
+      this.set("description", "");
+      this.set("employer", "");
+      this.set("title", "");
+    },
+    createChallenge() {
+      let { title, description, category, employer } = this.getProperties(
+        "title",
+        "description",
+        "category",
+        "employer"
+      );
+      console.log("Create Challenge: ", title, description, category, employer);
       //Include POST call somewhere to save challenge
+      this._super(...arguments);
+      this.set("isLoading", true);
+      $.ajax({
+        url: "http://api.tstfy.co/challenges",
+        type: "POST",
+        crossDomain: true,
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify({
+          description: description,
+          category: category,
+          employer: employer,
+          title: title
+        })
+      })
+        .then(resp => {
+          // handle your server response here
+          this.set("isLoading", false);
+          console.log("Create Challenge Response: ", resp);
+          if (!resp.repo_link) {
+            throw JSON.stringify(`Create Challenge Failed: ${resp}`);
+          } else {
+            this.toggleProperty("showNewChallenge");
+            this.clearFields();
+          }
+        })
+        .catch(error => {
+          // handle errors here
+          this.set("isLoading", false);
+          this.set("error", JSON.parse(error));
+          console.log("Create Challenge Error: ", JSON.parse(error));
+        });
     },
     logout() {
       this.get("session").invalidate();
