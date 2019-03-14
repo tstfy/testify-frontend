515a0b461c4531e765c884dafa14b5f6b320a20b
ykcai <lordsunday11@gmail.com>
2019-03-14 19:35:12 -0400

    added challenge details view, candidate table with updates

--- app/controllers/challenge.js
+++ app/controllers/challenge.js
@@ -5,6 +5,7 @@ import $ from "jquery";
 import { run } from "@ember/runloop";
 
 export default Controller.extend({
+  router: service(),
   showModalChallenge: false,
   showUpdateChallenge: false,
   showNewCandidate: false,
@@ -20,74 +21,76 @@ export default Controller.extend({
     },
     updateChallenge() {
       //TODO
-      let { title, description, category, employer } = this.getProperties(
-        "title",
-        "description",
-        "category",
-        "employer"
-      );
-      console.log("Create Challenge: ", title, description, category, employer);
-      //Include POST call somewhere to save challenge
-      this._super(...arguments);
-      this.set("isLoading", true);
-      $.ajax({
-        url: config.APP.baseURL + "/challenges",
-        type: "POST",
-        crossDomain: true,
-        contentType: "application/json;charset=UTF-8",
-        data: JSON.stringify({
-          description: description,
-          category: category,
-          employer: employer,
-          title: title
-        })
-      })
-        .then(resp => {
-          // handle your server response here
-          run(() => {
-            // begin loop
-            // Code that results in jobs being scheduled goes here
-            this.set("isLoading", false);
-            console.log("Create Challenge Response: ", resp);
-          }); // end loop, jobs are flushed and executed
-          if (!resp.repo_link) {
-            throw JSON.stringify(`Create Challenge Failed: ${resp}`);
-          } else {
-            this.toggleProperty("showModal");
-            this.toggleProperty("showNewChallenge");
-            this.send("clearFields");
-            this.refreshCurrentRoute();
-          }
-        })
-        .catch(error => {
-          // handle errors here
-          this.set("isLoading", false);
-          this.set("error", JSON.parse(error));
-          console.log("Create Challenge Error: ", JSON.parse(error));
-        });
+      // let { title, description, category, employer } = this.getProperties(
+      //   "title",
+      //   "description",
+      //   "category",
+      //   "employer"
+      // );
+      // console.log("Create Challenge: ", title, description, category, employer);
+      // //Include POST call somewhere to save challenge
+      // this._super(...arguments);
+      // this.set("isLoading", true);
+      // $.ajax({
+      //   url: config.APP.baseURL + "/challenges",
+      //   type: "POST",
+      //   crossDomain: true,
+      //   contentType: "application/json;charset=UTF-8",
+      //   data: JSON.stringify({
+      //     description: description,
+      //     category: category,
+      //     employer: employer,
+      //     title: title
+      //   })
+      // })
+      //   .then(resp => {
+      //     // handle your server response here
+      //     run(() => {
+      //       // begin loop
+      //       // Code that results in jobs being scheduled goes here
+      //       this.set("isLoading", false);
+      //       console.log("Create Challenge Response: ", resp);
+      //     }); // end loop, jobs are flushed and executed
+      //     if (!resp.repo_link) {
+      //       throw JSON.stringify(`Create Challenge Failed: ${resp}`);
+      //     } else {
+      //       this.toggleProperty("showModal");
+      //       this.toggleProperty("showNewChallenge");
+      //       this.send("clearFields");
+      //       this.refreshCurrentRoute();
+      //     }
+      //   })
+      //   .catch(error => {
+      //     // handle errors here
+      //     this.set("isLoading", false);
+      //     this.set("error", JSON.parse(error));
+      //     console.log("Create Challenge Error: ", JSON.parse(error));
+      //   });
     },
     newCandidate() {
-      //TODO
-      let { title, description, category, employer } = this.getProperties(
-        "title",
-        "description",
-        "category",
-        "employer"
-      );
-      console.log("Create Challenge: ", title, description, category, employer);
-      //Include POST call somewhere to save challenge
       this._super(...arguments);
+      console.log(this.model.candidates);
+      let { email, f_name, l_name } = this.getProperties(
+        "email",
+        "f_name",
+        "l_name"
+      );
+      console.log("Add Candidate: ", email, f_name, l_name);
+      //Include POST call somewhere to save challenge
       this.set("isLoading", true);
       $.ajax({
-        url: config.APP.baseURL + "/challenges",
+        url:
+          config.APP.baseURL +
+          `/challenges/${
+            this.session.data.authenticated.user.employer_id
+          }/candidates`,
         type: "POST",
         crossDomain: true,
         contentType: "application/json;charset=UTF-8",
         data: JSON.stringify({
-          description: description,
-          category: category,
-          employer: employer,
-          title: title
+          email: email,
+          f_name: f_name,
+          l_name: l_name
         })
       })
         .then(resp => {
@@ -96,22 +99,51 @@ export default Controller.extend({
             // begin loop
             // Code that results in jobs being scheduled goes here
             this.set("isLoading", false);
-            console.log("Create Challenge Response: ", resp);
+            console.log("Add Candidate Response: ", resp);
           }); // end loop, jobs are flushed and executed
-          if (!resp.repo_link) {
-            throw JSON.stringify(`Create Challenge Failed: ${resp}`);
+          if (!resp[0].last_modified) {
+            throw JSON.stringify(`Add Candidate Failed: ${resp}`);
           } else {
-            this.toggleProperty("showModal");
-            this.toggleProperty("showNewChallenge");
-            this.send("clearFields");
-            this.refreshCurrentRoute();
+            this.send("toggleNewCandidate");
+            this.send("refreshModel");
           }
         })
         .catch(error => {
           // handle errors here
           this.set("isLoading", false);
           this.set("error", JSON.parse(error));
-          console.log("Create Challenge Error: ", JSON.parse(error));
+          console.log("Add Candidate Error: ", JSON.parse(error));
+        });
+    },
+    copyToClipboard(text) {
+      var dummyElement = document.createElement("input");
+      document.body.appendChild(dummyElement);
+      dummyElement.setAttribute("value", text);
+      dummyElement.select();
+      document.execCommand("copy");
+      document.body.removeChild(dummyElement);
+    },
+    refreshModel: function() {
+      this._super(...arguments);
+      $.ajax({
+        url:
+          config.APP.baseURL +
+          "/challenges/candidates?cid=" +
+          this.model.challenges.challenge_id,
+        type: "GET",
+        crossDomain: true,
+        contentType: "application/json;charset=UTF-8"
+      })
+        .then(resp => {
+          this.set("num_candidates", resp.data.length);
+          console.log("num_candidates ", this.num_candidates);
+          this.get("store").pushPayload("candidate", resp);
+        })
+        .catch(error => {
+          // handle errors here
+          this.set("isLoading", false);
+          this.set("error", JSON.parse(error));
+          console.log("Refresh Candidate Error: ", JSON.parse(error));
         });
     }
   }
--- app/routes/challenge.js
+++ app/routes/challenge.js
@@ -13,10 +13,12 @@ export default Route.extend(AuthenticatedRouteMixin, {
           eid: this.session.data.authenticated.user.employer_id
         })
         .then(challenges => {
-          challenges.filter(challenge => {
-            this.set("challengeid", challenge.challengeid);
-            return params.id === challenge.challengeid;
-          });
+          return challenges
+            .filter(challenge => {
+              this.set("challengeid", challenge.challengeid);
+              return params.id === challenge.challengeid;
+            })
+            .get("firstObject");
         }),
       candidates: $.ajax({
         url:
@@ -25,8 +27,16 @@ export default Route.extend(AuthenticatedRouteMixin, {
         crossDomain: true,
         contentType: "application/json;charset=UTF-8"
       }).then(resp => {
-        return resp;
+        this.set("num_candidates", resp.data.length);
+        console.log("num_candidates ", this.num_candidates);
+        this.get("store").pushPayload("candidate", resp);
+        return this.get("store").peekAll("candidate");
       })
     });
+  },
+  actions: {
+    refreshCurrentRoute() {
+      this.refresh();
+    }
   }
 });
