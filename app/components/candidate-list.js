6306025d07b96ed7f8338d4b50dda776c98b849c
Michael Cai <ykcai@uwaterloo.ca>
2019-03-14 23:43:07 -0400

    invited added, disable is status is not 0

--- app/components/candidate-list.js
+++ app/components/candidate-list.js
@@ -1,3 +1,55 @@
 import Component from "@ember/component";
+import $ from "jquery";
+import { inject as service } from "@ember/service";
+import { run } from "@ember/runloop";
+import config from "../config/environment";
 
-export default Component.extend({});
+export default Component.extend({
+  router: service(),
+  actions: {
+    copyToClipboard(text) {
+      var dummyElement = document.createElement("input");
+      document.body.appendChild(dummyElement);
+      dummyElement.setAttribute("value", text);
+      dummyElement.select();
+      document.execCommand("copy");
+      document.body.removeChild(dummyElement);
+    },
+    inviteCandidate(candidate_id, employer_id) {
+      console.log("Invite Candidate: ", candidate_id, this.router);
+      this._super(...arguments);
+      $.ajax({
+        url: config.APP.baseURL + `/challenges/${employer_id}/invite`,
+        type: "POST",
+        crossDomain: true,
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify({
+          candidate_ids: [candidate_id],
+          employer_id: employer_id
+        })
+      })
+        .then((resp, textStatus, xhr) => {
+          // handle your server response here
+          run(() => {
+            // begin loop
+            // Code that results in jobs being scheduled goes here
+            console.log("Invite Candidate Response: ", resp, textStatus, xhr);
+          }); // end loop, jobs are flushed and executed
+          if (xhr.status != 200) {
+            throw JSON.stringify(`Invite Candidate Failed: ${resp}`);
+          } else {
+            this.toggleProperty("showModal");
+            this.toggleProperty("showNewChallenge");
+            this.send("clearFields");
+            this.refreshCurrentRoute();
+          }
+        })
+        .catch(error => {
+          // handle errors here
+          this.set("isLoading", false);
+          this.set("error", JSON.parse(error));
+          console.log("Invite Candidate Error: ", JSON.parse(error));
+        });
+    }
+  }
+});
--- app/controllers/challenge.js
+++ app/controllers/challenge.js
@@ -21,69 +21,21 @@ export default Controller.extend({
     },
     updateChallenge() {
       //TODO
-      // let { title, description, category, employer } = this.getProperties(
-      //   "title",
-      //   "description",
-      //   "category",
-      //   "employer"
-      // );
-      // console.log("Create Challenge: ", title, description, category, employer);
-      // //Include POST call somewhere to save challenge
-      // this._super(...arguments);
-      // this.set("isLoading", true);
-      // $.ajax({
-      //   url: config.APP.baseURL + "/challenges",
-      //   type: "POST",
-      //   crossDomain: true,
-      //   contentType: "application/json;charset=UTF-8",
-      //   data: JSON.stringify({
-      //     description: description,
-      //     category: category,
-      //     employer: employer,
-      //     title: title
-      //   })
-      // })
-      //   .then(resp => {
-      //     // handle your server response here
-      //     run(() => {
-      //       // begin loop
-      //       // Code that results in jobs being scheduled goes here
-      //       this.set("isLoading", false);
-      //       console.log("Create Challenge Response: ", resp);
-      //     }); // end loop, jobs are flushed and executed
-      //     if (!resp.repo_link) {
-      //       throw JSON.stringify(`Create Challenge Failed: ${resp}`);
-      //     } else {
-      //       this.toggleProperty("showModal");
-      //       this.toggleProperty("showNewChallenge");
-      //       this.send("clearFields");
-      //       this.refreshCurrentRoute();
-      //     }
-      //   })
-      //   .catch(error => {
-      //     // handle errors here
-      //     this.set("isLoading", false);
-      //     this.set("error", JSON.parse(error));
-      //     console.log("Create Challenge Error: ", JSON.parse(error));
-      //   });
     },
     newCandidate() {
       this._super(...arguments);
-      console.log(this.model);
+      console.log(this.model, this.model.challenges.challenge_id);
       let { email, f_name, l_name } = this.getProperties(
         "email",
         "f_name",
         "l_name"
       );
       console.log("Add Candidate: ", email, f_name, l_name);
-      //Include POST call somewhere to save challenge
       this.set("isLoading", true);
       $.ajax({
         url:
           config.APP.baseURL +
-          `/challenges/${
-            this.session.data.authenticated.user.employer_id
-          }/candidates`,
+          `/challenges/${this.model.challenges.challenge_id}/candidates`,
         type: "POST",
         crossDomain: true,
         contentType: "application/json;charset=UTF-8",
@@ -93,13 +45,13 @@ export default Controller.extend({
           l_name: l_name
         })
       })
-        .then(resp => {
+        .then((resp, textStatus, xhr) => {
           // handle your server response here
           run(() => {
             // begin loop
             // Code that results in jobs being scheduled goes here
             this.set("isLoading", false);
-            console.log("Add Candidate Response: ", resp);
+            console.log("Add Candidate Response: ", resp, textStatus, xhr);
           }); // end loop, jobs are flushed and executed
           if (!resp[0].last_modified) {
             throw JSON.stringify(`Add Candidate Failed: ${resp}`);
@@ -115,18 +67,32 @@ export default Controller.extend({
           console.log("Add Candidate Error: ", JSON.parse(error));
         });
     },
-    copyToClipboard(text) {
-      var dummyElement = document.createElement("input");
-      document.body.appendChild(dummyElement);
-      dummyElement.setAttribute("value", text);
-      dummyElement.select();
-      document.execCommand("copy");
-      document.body.removeChild(dummyElement);
-    },
-    refreshModel: function(challengeid) {
+    inviteAllCandidates(candidates) {
+      let invites = [];
+      candidates.forEach(candidate => {
+        invites.push(candidate.candidate_id);
+      });
+      console.log("Invite All Candidate: ", invites);
       this._super(...arguments);
       $.ajax({
-        url: config.APP.baseURL + "/challenges/candidates?cid=" + challengeid,
+        url:
+          config.APP.baseURL +
+          `/challenges/${
+            this.session.data.authenticated.user.employer_id
+          }/invite`,
+        type: "POST",
+        crossDomain: true,
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify({
+          candidate_ids: invites,
+          employer_id: this.session.data.authenticated.user.employer_id
+        })
+      });
+    },
+    refreshModel: function(challenge_id) {
+      this._super(...arguments);
+      $.ajax({
+        url: config.APP.baseURL + "/challenges/candidates?cid=" + challenge_id,
         type: "GET",
         crossDomain: true,
         contentType: "application/json;charset=UTF-8"
