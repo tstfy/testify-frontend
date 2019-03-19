de8c4a80ff8750b95dffb69a309af869c7b9488c
Michael Cai <ykcai@uwaterloo.ca>
2019-03-19 03:38:02 -0400

    added git view list drop down took me fking 6 hours

--- app/components/candidate-list.js
+++ app/components/candidate-list.js
@@ -6,10 +6,42 @@ import config from "../config/environment";
 
 export default Component.extend({
   router: service(),
+  panelActions: service(),
+  store: service(),
   snackText: "",
+  toggleLoadingProgress(candidate_id) {
+    const view = document.getElementById(`view-${candidate_id}`);
+    const loading = document.getElementById(`loading-${candidate_id}`);
+
+    if (view.style.display === "none") {
+      view.style.display = "table-cell";
+    } else {
+      view.style.display = "none";
+    }
+    if (loading.style.display === "none") {
+      loading.style.display = "table-cell";
+    } else {
+      loading.style.display = "none";
+    }
+  },
+  toggleLoadingInvite(candidate_id) {
+    const view = document.getElementById(`view-${candidate_id}`);
+    const loading = document.getElementById(`loading-${candidate_id}`);
+
+    if (view.style.display === "none") {
+      view.style.display = "table-cell";
+    } else {
+      view.style.display = "none";
+    }
+    if (loading.style.display === "none") {
+      loading.style.display = "table-cell";
+    } else {
+      loading.style.display = "none";
+    }
+  },
   actions: {
     snackText(text) {
-      var x = document.getElementById("snackbar");
+      const x = document.getElementById("snackbar");
       this.set("snackText", text);
       // Add the "show" class to DIV
       x.className = "show";
@@ -20,7 +52,7 @@ export default Component.extend({
       }, 3000);
     },
     copyToClipboard(text) {
-      var dummyElement = document.createElement("input");
+      const dummyElement = document.createElement("input");
       document.body.appendChild(dummyElement);
       dummyElement.setAttribute("value", text);
       dummyElement.select();
@@ -73,6 +105,7 @@ export default Component.extend({
     statusChanged(challenge_id, candidate_id, status) {
       console.log(challenge_id, candidate_id, status.target.value);
       this._super(...arguments);
+      this.toggleLoadingProgress(candidate_id);
       $.ajax({
         url:
           config.APP.baseURL +
@@ -94,16 +127,64 @@ export default Component.extend({
           if (xhr.status != 200) {
             throw JSON.stringify(`Updated Status Failed: ${resp}`);
           } else {
+            this.toggleLoadingProgress(candidate_id);
             this.send("snackText", `Updated Status`);
             this.refreshCurrentRoute();
           }
         })
         .catch(error => {
           // handle errors here
-          this.set("isLoading", false);
+          this.toggleLoadingProgress(candidate_id);
           this.send("snackText", `Updated Status Error JSON.parse(error)`);
           this.set("error", JSON.parse(error));
           console.log("Invite Candidate Error: ", JSON.parse(error));
+        });
+    },
+    getProgression(challenge_id, candidate_id) {
+      this._super(...arguments);
+      this.toggleLoadingProgress(candidate_id);
+      $.ajax({
+        url:
+          config.APP.baseURL +
+          `/challenges/${challenge_id}/candidates/${candidate_id}`,
+        type: "GET",
+        crossDomain: true,
+        contentType: "application/json;charset=UTF-8"
+      })
+        .then((resp, textStatus, xhr) => {
+          // handle your server response here
+          run(() => {
+            // begin loop
+            // Code that results in jobs being scheduled goes here
+            console.log("Git Pregression Response: ", resp, textStatus, xhr);
+          }); // end loop, jobs are flushed and executed
+          if (!resp.data) {
+            throw JSON.stringify(`Git Pregression Failed: ${resp}`);
+          } else {
+            this.store
+              .findRecord("candidate", candidate_id)
+              .then(function(candidate) {
+                candidate.gitlogs = resp;
+                console.log("FOUND: ", candidate);
+              });
+            this.toggleLoadingProgress(candidate_id);
+            this.get("panelActions").toggle(candidate_id);
+            const x = document.getElementsByClassName(
+              `progression-${candidate_id}`
+            );
+            if (x[0].style.display === "none") {
+              x[0].style.display = "table-row";
+            } else {
+              x[0].style.display = "none";
+            }
+          }
+        })
+        .catch(error => {
+          // handle errors here
+          this.toggleLoadingProgress(candidate_id);
+          this.send("snackText", `Git Pregression Error ${JSON.parse(error)}`);
+          this.set("error", JSON.parse(error));
+          console.log("Git Pregression Error: ", JSON.parse(error));
         });
     }
   }
